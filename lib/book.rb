648e267cd13d02ba800d5c592be7a2029c33a1ab
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-31 03:43:03 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,12 @@
 class Book
-  attr_accessor :title, :author, :page_count, :genre, :turn_page
-
+  attr_accessor :title, :author, :page_count, :genre,
 
   def initialize(title)
     @title = title
   end
 
+def turn_page
+
+end
 
 end
