5550ad2d89cee23030b210b541fbd1b19351f96f
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-31 04:04:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,17 +1,17 @@
 # Make your shoe class here!
 class Shoe
   attr_reader :shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition, :brand
 
   def initialize(shoe)
     @shoe = shoe
   end
-  
-  def brand=(brand="Nike")
-    @brand = brand
-  end
 
-  def brand
-    @brand
-  end
+  # def brand=(brand="Nike")
+  #   @brand = brand
+  # end
+  #
+  # def brand
+  #   @brand
+  # end
 end
