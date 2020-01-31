02a00644b23451dc1e128f2dcbe98cda438d4fab
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-31 04:19:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,7 +10,13 @@ class Shoe
 
   def cobble
     puts "Your shoe is as good as new!"
+    shoe.condition = "new"
   end
+
+def condition(arg)
+  puts arg
+end
+
 
   # def brand=(brand="Nike")
   #   @brand = brand
